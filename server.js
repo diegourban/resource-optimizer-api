038c639fef6d47e8178970e29bb30c0ded3fac41
Diego Leonardo Urban <urban@dynamix.com.br>
2017-10-20 11:05:58 -0200

    handling startup error

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const logger = require('./config/log');
 const app = require('./config/express')();
 
-var server = app.listen(3000, function() {
+var server = app.listen(3000, function(error) {
+  if (error) {
+    logger.error("Não foi possível iniciar a conexão", error);
+    process.exit(10);
+  }
+  
   logger.info("Servidor iniciado, ouvindo na porta 3000");
 })
 .on("error", function(err){
