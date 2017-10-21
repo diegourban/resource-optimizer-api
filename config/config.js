f76530d6ef0517b61fd8378afa9aaf0f26703d13
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-10-21 10:35:25 -0200

    configs and log

---
+++ config/config.js
@@ -0,0 +1,6 @@
+var config = module.exports
+
+config.express = {
+  port: process.env.EXPRESS_PORT || 3000,
+  ip: "localhost"
+}
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const logger = require('./config/log');
-const app = require('./config/express')();
+const logger = require("./config/log");
+const app = require("./config/express")();
+const config = require("./config/config");
 
-var server = app.listen(3000, function(error) {
+app.listen(config.express.port, config.express.ip, function(error) {
   if (error) {
     logger.error("Não foi possível iniciar a conexão", error);
     process.exit(10);
   }
-  
-  logger.info("Servidor iniciado, ouvindo na porta 3000");
+
+  logger.info(`Servidor iniciado em ${config.express.ip}, ouvindo na porta ${config.express.port}`);
 })
-.on("error", function(err){
-  logger.error("Erro inesperado : " + err);
+.on("error", function(error) {
+  logger.error("Erro inesperado : " + error);
 });
