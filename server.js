646b067194be7815afbcd9fb33169fcc530d3d98
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-10-28 09:12:20 -0200

    teste de start

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const logger = require("./config/log");
 const app = require("./config/express")();
 const config = require("./config/config");
 
-app.listen(config.express.port, config.express.ip, function(error) {
+app.listen(config.express.port, function(error) {
   if (error) {
     logger.error("Não foi possível iniciar a conexão", error);
     process.exit(10);
   }
 
-  logger.info(`Servidor iniciado em ${config.express.ip}, ouvindo na porta ${config.express.port}`);
+  logger.info(`Servidor iniciado na porta ${config.express.port}`);
 })
 .on("error", function(error) {
   logger.error("Erro inesperado : " + error);
