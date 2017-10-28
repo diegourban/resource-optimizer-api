19b73dfdf57e30e69083b0f286588cdc4d6e9bc3
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-10-28 09:45:09 -0200

    log

--- config/log.js
+++ config/log.js
@@ -2,16 +2,11 @@ const fs = require("fs");
 const winston = require("winston");
 const logfile = "api.log";
 
-if(!fs.existsSync(logfile)) {
-  fs.writeFile(logfile, "", (err) => {
-    if (err) throw err;
-    console.log("arquivo criado");
-  });
-}
 winston.add(winston.transports.File, { filename: logfile });
 
 if (process.env.NODE_ENV === "production") {
-  winston.remove(winston.transports.Console);
+  // Heroku não permite criar arquivos
+  //winston.remove(winston.transports.Console);
 }
 
 module.exports = winston;
