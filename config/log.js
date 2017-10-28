f67f81f2ed8c608200f19a6c8f8edd2ca9c6710d
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-10-28 09:41:09 -0200

    teste criação arquivo

--- config/log.js
+++ config/log.js
@@ -1,9 +1,17 @@
+const fs = require("fs");
 const winston = require("winston");
+const logfile = "api.log";
 
-winston.add(winston.transports.File, { filename: "api.log" });
+if(!fs.existsSync(logfile)) {
+  fs.writeFile(logfile, "", (err) => {
+    if (err) throw err;
+    console.log("arquivo criado");
+  });
+}
+winston.add(winston.transports.File, { filename: logfile });
 
 if (process.env.NODE_ENV === "production") {
-  //winston.remove(winston.transports.Console);
+  winston.remove(winston.transports.Console);
 }
 
 module.exports = winston;
